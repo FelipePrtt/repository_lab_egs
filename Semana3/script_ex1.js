e7a5578de920ae1363a652aac65e179b4d0e7ae7
Felipe Pretti <felipepretti2001@gmail.com>
2025-02-24 23:51:13 -0300

    Exercício 1 Semana 3

--- Semana3/script_ex1.js
+++ Semana3/script_ex1.js
@@ -1,3 +1,21 @@
-function atualizarArray(event){
+let arrayDados = [];
 
+function atualizarArray(event) {
+    event.preventDefault();
+
+    let dado = document.getElementById('dado').value;
+    arrayDados.push(dado); //insere o dado
+    arrayDados.sort(); //ordena o array
+
+    let lista = document.getElementById('listaDados'); //a variavel lista recebe o conteudo da tag ul (que possui o id listaDados)
+
+    lista.innerHTML = ''; //define o conteudo da lista como ""vazio""
+
+    arrayDados.forEach(function(dado) {
+        let li = document.createElement('li');
+        li.textContent = dado;
+        lista.appendChild(li);
+    });
+
+    document.getElementById('dado').value = '';
 }
